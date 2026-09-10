d0b174c29b10b9d00c2fbbc53e72c7f676f337e8
ioanatu <tatu.ioana@gmail.com>
2026-09-10 22:19:01 +0200

    Test

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [react()],
     build: {
-      sourcemap: 'hidden',
+      sourcemap: true,
       rolldownOptions: {
         output: {
           manualChunks(id: string) {
